c2e712dc5379caa95d4dda9f2b7cfb8b6f210e74
akrala <akrala@yahoo.com>
2021-04-29 22:13:30 -0500

    more tests

--- godash_test.go
+++ godash_test.go
@@ -5,14 +5,44 @@ import (
 	"testing"
 )
 
-var testingData = []string{"peach", "apple", "pear", "plum"}
+type Point struct {
+	X int
+	Y int
+}
+type Points []Point
 
-func TestFirst(t *testing.T) {
-	target := testingData[0]
+var stringArray = []string{"peach", "apple", "pear", "plum"}
+var intArray = []int{1, 2, 3, 4}
+var structArray = Points{
+	{0, 0},
+	{1, 0},
+	{0, 1},
+}
+
+func TestFirstString(t *testing.T) {
+	target := stringArray[0]
 	want := regexp.MustCompile(target)
-	f := First(testingData)
+	f := First(stringArray)
 
 	if f == nil || !want.MatchString(f.(string)) {
+		t.Fatalf(`does not match`)
+	}
+}
+
+func TestFirstInt(t *testing.T) {
+	target := intArray[0]
+	f := First(intArray)
+
+	if f == nil || target != f {
+		t.Fatalf(`does not match`)
+	}
+}
+
+func TestFirstPoint(t *testing.T) {
+	target := structArray[0]
+	f := First(structArray)
+
+	if f == nil || target != f {
 		t.Fatalf(`does not match`)
 	}
 }
